9ffc6614ba0eb9b52c849a3c2b608ecb8c18872b
Christopher Razidlo <cd.razidlo@gmail.com>
2022-03-29 11:26:20 -0500

    App test added, need to get render working to run tests on DOM

---
+++ src/components/App/App.test.js
@@ -0,0 +1,12 @@
+import App from './App';
+import React from 'react';
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("App", () => {
+    test("Route component should load", () => {
+        // TODO: Render app, check for text of routeselect child to make sure it rendered
+        // render(<App />);
+        // expect(getByText(/Select a Route/i)).toBeInTheDocument();
+    })
+})
